8c4d5cec3ad1d1bd700b03deba9f998a5070d612
Golden-Lombe <goldenl@ozow.com>
2021-03-03 09:38:19 +0200

    Add project files.

---
+++ BiggestNum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BiggestNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The largest number is:" + biggestNumber());
+        }
+
+        static int[] array = { 190, 291, 145, 209, 280, 300 };
+
+        public static int biggestNumber()
+        {
+
+            var maxNumber = array[0];
+
+            var i = 0;
+
+            for (i = 1; i < array.Length; i++)
+                if (array[i] > maxNumber)
+                    maxNumber = array[i];
+
+            return maxNumber;
+        }
+    }
+}
